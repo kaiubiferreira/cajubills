9a92769bcffd00dd386aa5cb1b42d3327d60bd2b
Kaiubi Ferreira <kaiubiferreira@gmail.com>
2023-10-09 13:23:59 -0300

    Update variable income input

---
+++ sql/ddl/hot_start.sql
@@ -0,0 +1,7 @@
+INSERT INTO fixed_income (asset, purchaseDate, dueDate, financialIndex, value, preRate, postRate) VALUES
+('Banco Xp Previdencia', '2021-11-30', '2041-11-30', 'cdi', 33000, 0, 1.10),
+('Banco Fibra Pre 13.75%', '2018-06-15', '2023-06-15', 'pre', 10000, 0.1375, 0),
+('Banco Master IPCA + 5.96%', '2021-10-24', '2026-10-28', 'ipca', 25000, 1, 0.0596),
+('Banco Master Pre 11%', '2020-09-22', '2028-08-14', 'pre', 25000, 0.11, 0),
+('Banco Master Pos 140% Cdi', '2020-09-21', '2028-08-14', 'cdi', 25000, 0, 1.4),
+('Banco C6 IPCA + 5.70%', '2021-09-06', '2028-09-06', 'ipca', 25000, 1, 0.057);
